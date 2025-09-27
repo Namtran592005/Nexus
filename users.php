5212262c9813b9ac980bccf8e1b1661f15c030e2
Nam Trần <Namtran5905@gmail.com>
2025-09-28 06:51:54 +0700

    Update users.php

--- users.php
+++ users.php
@@ -3,10 +3,8 @@
 return array (
   'admin' => 
   array (
-    'password' => '$2a$12$RIUD80yRYDJanSoRF54VeeHvAelKypWT8Z3PkwMqk3df1gKBcRSB2',
+    'password' => '$2a$12$YJRMUf9cArYjirU2WWnVRuqZ/lcr4fzj2ppuFDGqXKeXujE1NNJ96',
     'google2fa_secret' => NULL,
     'google2fa_enabled' => false,
-    'tfa_enabled' => true,
-    'tfa_secret' => 'FH4DNAD7J7DZYNYSW6363TK7QL7K36HT',
   ),
 );
